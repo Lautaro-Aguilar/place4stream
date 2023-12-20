f41df9fa3db44184b17bb8746e986c2ae6a8e87c
Lautaro Aguilar <aguilarlautaroexequiel867@gmail.com>
2023-12-19 22:16:46 -0300

    lazy loading implemented

--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -1,13 +1,17 @@
-import { Navbar } from "./_components/navbar";
-import Sidebar from "./_components/sidebar";
-import { Container } from "./_components/container";
+import { Suspense } from 'react';
+
+import { Navbar } from './_components/navbar';
+import Sidebar, { SidebarSkeleton } from './_components/sidebar';
+import { Container } from './_components/container';
 
 export const BrowseLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Navbar />
-      <div className="flex h-full pt-20">
-        <Sidebar />
+      <div className='flex h-full pt-20'>
+        <Suspense fallback={<SidebarSkeleton />}>
+          <Sidebar />
+        </Suspense>
         <Container>{children}</Container>
       </div>
     </>
