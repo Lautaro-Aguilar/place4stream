daf1eaaa2eb83308e583b6a38ebe096f144538f1
Lautaro Aguilar <aguilarlautaroexequiel867@gmail.com>
2023-12-25 22:52:38 -0300

    add block action for test

--- app/(browse)/[username]/_components/actions.tsx
+++ app/(browse)/[username]/_components/actions.tsx
@@ -3,6 +3,7 @@
 import { toast } from 'sonner';
 import { useTransition } from 'react';
 
+import { onBlock } from '@/actions/block';
 import { onFollow, onUnfollow } from '@/actions/follow';
 import { Button } from '@/components/ui/button';
 
@@ -36,13 +37,31 @@ export const Actions = ({ isFollowing, userId }: ActionsProps) => {
 
   const onClick = isFollowing ? handleUnfollow : handleFollow;
 
+  const handleBlock = () => {
+    startTransition(() => {
+      onBlock(userId)
+        .then((data) =>
+          toast.success(`You have blocked ${data.blocked.username}`)
+        )
+        .catch(() => toast.error('Something went wrong'));
+    });
+  };
+
   return (
-    <Button
-      disabled={isPending}
-      onClick={onClick}
-      variant='primary'
-    >
-      {isFollowing ? 'Unfollow' : 'Follow'}
-    </Button>
+    <>
+      <Button
+        disabled={isPending}
+        onClick={onClick}
+        variant='primary'
+      >
+        {isFollowing ? 'Unfollow' : 'Follow'}
+      </Button>
+      <Button
+        onClick={handleBlock}
+        disabled={isPending}
+      >
+        Block User
+      </Button>
+    </>
   );
 };
