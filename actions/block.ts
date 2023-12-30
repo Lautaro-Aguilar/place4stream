57f2dad89254017f19ce4679d8a504d1172961e4
Lautaro Aguilar <aguilarlautaroexequiel867@gmail.com>
2023-12-30 17:30:22 -0300

    action is modified to block/expel guest's

--- actions/block.ts
+++ actions/block.ts
@@ -1,17 +1,34 @@
 'use server';
 
 import { revalidatePath } from 'next/cache';
+import { RoomServiceClient } from 'livekit-server-sdk';
 
 import { blockUser, unblockUser } from '@/lib/block-service';
+import { getSelf } from '@/lib/auth-service';
+
+const roomService = new RoomServiceClient(
+  process.env.LIVEKIT_API_URL!,
+  process.env.LIVEKIT_API_KEY!,
+  process.env.LIVEKIT_API_SECRET!
+);
 
 export const onBlock = async (id: string) => {
-  //TODO: Adapt to disconnect from livestream
-  //TODO: Allow ability to kick the guest
-  const blockedUser = await blockUser(id);
+  const self = await getSelf();
 
-  if (blockedUser) {
-    revalidatePath(`/${blockedUser.blocked.username}`);
+  let blockedUser;
+  try {
+    blockedUser = await blockUser(id);
+  } catch {
+    // user is a guest
   }
+
+  try {
+    await roomService.removeParticipant(self.id, id);
+  } catch {
+    // this user is not in the room
+  }
+
+  revalidatePath(`/u/${self.username}/community`);
 
   return blockedUser;
 };
