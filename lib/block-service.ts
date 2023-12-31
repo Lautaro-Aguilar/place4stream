4154710bbeac4805bd6fc108233807abe0168a51
Lautaro Aguilar <aguilarlautaroexequiel867@gmail.com>
2023-12-31 15:59:50 -0300

    update services

--- lib/block-service.ts
+++ lib/block-service.ts
@@ -1,4 +1,4 @@
-import { db } from '@/lib//db';
+import { db } from '@/lib/db';
 import { getSelf } from '@/lib/auth-service';
 
 export const isBlockedByUser = async (id: string) => {
@@ -111,4 +111,19 @@ export const unblockUser = async (id: string) => {
   });
 
   return unblock;
+};
+
+export const getBlockedUsers = async () => {
+  const self = await getSelf();
+
+  const blockedUsers = await db.block.findMany({
+    where: {
+      blockerId: self.id,
+    },
+    include: {
+      blocked: true,
+    },
+  });
+
+  return blockedUsers;
 };
